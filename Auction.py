c7b92887d9aa8b29f15f1b28276c4982938f4035
sanketnighot <sanketnighot25@gmail.com>
2023-03-26 14:53:13 +0530

    Minor bugs fixes and code improvement in Auctions Smart contract

--- Auction.py
+++ Auction.py
@@ -30,6 +30,23 @@ class AuctionData:
             highest_bidder = _params.highest_bidder
         )
 
+class Batch_transfer:
+    def get_transfer_type():
+        tx_type = sp.TRecord(to_=sp.TAddress,
+                             token_id=sp.TNat,
+                             amount=sp.TNat)
+        transfer_type = sp.TRecord(from_=sp.TAddress,
+                                   txs=sp.TList(tx_type)).layout(
+                                       ("from_", "txs"))
+        return transfer_type
+
+    def get_type():
+        return sp.TList(Batch_transfer.get_transfer_type())
+
+    def item(from_, txs):
+        v = sp.record(from_=from_, txs=txs)
+        return sp.set_type_expr(v, Batch_transfer.get_transfer_type())
+
 class Auction(sp.Contract):
     def __init__(self, _mod):
         self.init(
@@ -38,6 +55,34 @@ class Auction(sp.Contract):
             auctions = AuctionData().set_type(),
             pause = sp.bool(False)
         )
+        
+    def transfer_token(self, contract, params_):
+        sp.set_type(contract, sp.TAddress)
+        sp.set_type(params_, sp.TList(
+                sp.TRecord(
+                    from_ = sp.TAddress, 
+                    txs = sp.TList(
+                        sp.TRecord(
+                            amount = sp.TNat, 
+                            to_ = sp.TAddress, 
+                            token_id = sp.TNat
+                        ).layout(("to_", ("token_id", "amount")))
+                    )
+                )
+            .layout(("from_", "txs"))))
+        contractParams = sp.contract(sp.TList(
+                sp.TRecord(
+                    from_ = sp.TAddress, 
+                    txs = sp.TList(
+                        sp.TRecord(
+                            amount = sp.TNat, 
+                            to_ = sp.TAddress, 
+                            token_id = sp.TNat
+                        ).layout(("to_", ("token_id", "amount")))
+                    )
+                )
+            .layout(("from_", "txs"))), contract, entry_point="transfer").open_some()
+        sp.transfer(params_, sp.mutez(0), contractParams)
     
     @sp.entry_point
     def add_moderator(self, _moderator):
@@ -60,6 +105,15 @@ class Auction(sp.Contract):
         sp.verify(_params.creator == sp.sender, "INVALID_CREATOR")
         self.data.auctions[self.data.next_auction_id] = AuctionData().set_value(_params)
         self.data.next_auction_id += sp.nat(1)
+        params = [
+                Batch_transfer.item(from_=sp.sender,
+                                       txs=[
+                                           sp.record(to_=sp.self_address,
+                                                     amount=1,
+                                                     token_id=_params.token.token_id)
+                                       ])
+            ]
+        self.transfer_token(_params.token.address, params)
         sp.emit(sp.record(token=_params.token.address,token_id=_params.token.token_id),tag="AUCTION_CREATED")
         
     @sp.entry_point
@@ -69,6 +123,16 @@ class Auction(sp.Contract):
         sp.verify(self.data.auctions[auction_id].creator == sp.sender, "INVALID_CREATOR")
         sp.if self.data.auctions[auction_id].current_price > sp.tez(0):
             sp.send(self.data.auctions[auction_id].highest_bidder, self.data.auctions[auction_id].current_price)
+        _params = [
+                Batch_transfer.item(from_=sp.self_address,
+                                       txs=[
+                                           sp.record(to_=self.data.auctions[auction_id].creator,
+                                                     amount=1,
+                                                     token_id=self.data.auctions[auction_id].token.token_id)
+                                       ])
+            ]
+        self.transfer_token(self.data.auctions[auction_id].token.address, _params)
+        del self.data.auctions[auction_id]
         sp.emit(sp.record(auction_id=auction_id,tag="AUCTION_CANCELED"))
     
     @sp.entry_point
@@ -78,6 +142,8 @@ class Auction(sp.Contract):
         sp.verify(sp.amount >= self.data.auctions[auction_id].current_price + self.data.auctions[auction_id].price_increment, "INSUFFICIENT_AMOUNT")
         sp.verify(sp.now >= self.data.auctions[auction_id].start_time, "AUCTION_NOT_STARTED")
         sp.verify(sp.now <= self.data.auctions[auction_id].end_time, "AUCTION_ENDED")
+        sp.if self.data.auctions[auction_id].highest_bidder != self.data.auctions[auction_id].creator:
+            sp.send(self.data.auctions[auction_id].highest_bidder, self.data.auctions[auction_id].current_price)
         self.data.auctions[auction_id].current_price = sp.amount
         self.data.auctions[auction_id].highest_bidder = sp.sender
         sp.emit(sp.record(token=self.data.auctions[auction_id].token.address,token_id=self.data.auctions[auction_id].token.token_id,bid=sp.amount,bidder=sp.sender),tag="NEW_BID")
@@ -86,6 +152,18 @@ class Auction(sp.Contract):
     def settle_auction(self, auction_id):
         sp.set_type(auction_id, sp.TNat)
         sp.verify(self.data.auctions.contains(auction_id), "INVALID_AUCTION_ID")
+        _params = [
+                Batch_transfer.item(from_=sp.self_address,
+                                       txs=[
+                                           sp.record(to_=self.data.auctions[auction_id].highest_bidder,
+                                                     amount=1,
+                                                     token_id=self.data.auctions[auction_id].token.token_id)
+                                       ])
+            ]
+        self.transfer_token(self.data.auctions[auction_id].token.address, _params)
+        sp.send(self.data.auctions[auction_id].creator, self.data.auctions[auction_id].current_price)
+        del self.data.auctions[auction_id]
+        sp.emit(sp.record(auction_id=auction_id,tag="AUCTION_SETTLED"))
 
     @sp.entry_point
     def toggle_pause(self):
@@ -96,20 +174,23 @@ class Auction(sp.Contract):
 @sp.add_test(name="Auction")
 def test():
     sc = sp.test_scenario()
+    
     sc.h1("Quilt NFT Collection Aucitons")
     sc.table_of_contents()
-    admin = sp.test_account("Admin")
-    alice = sp.test_account("Alice")
-    bob = sp.test_account("Bob")
-    elon = sp.test_account("Elon")
-    mark = sp.test_account("Mark")
+    admin = sp.address("tz1ADMINoooooXqTzhz67QYVPJAU9Y2g48kq")
+    alice = sp.address("tz1ALICEoooooXqTzhz67QYVPJAU9Y2g48kq")
+    bob = sp.address("tz1BOBoooCkrBkXqTzhz67QYVPJAU9Y2g48kq")
+    elon = sp.address("tz1ELONooooooXqTzhz67QYVPJAU9Y2g48kq")
+    mark = sp.address("tz1MARKoooBkXqTzhz67QYVPJAU9Y2g48kq")
     sc.show([admin, alice, bob, mark, elon, ])
+    
     sc.h1("Code")
-    auc = Auction(admin.address)
+    auc = Auction(admin)
     sc += auc
+    
     sc.h1("Create Auction")
     auc_data = sp.record(
-            creator = alice.address,
+            creator = alice,
             token = sp.record(
                 address = sp.address("KT1TezoooozzSmartPyzzDYNAMiCzzpLu4LU"),
                 token_id = sp.nat(0)
@@ -118,11 +199,11 @@ def test():
             end_time = sp.timestamp(10),
             price_increment = sp.tez(1),
             current_price = sp.tez(0),
-            highest_bidder = alice.address
+            highest_bidder = alice
         )
-    sc += auc.create_auction(auc_data).run(sender = alice.address)
+    sc += auc.create_auction(auc_data).run(sender = alice)
     auc_data = sp.record(
-            creator = bob.address,
+            creator = bob,
             token = sp.record(
                 address = sp.address("KT1Tezooo1zzSmartPyzzDYNAMiCzzpLu4LU"),
                 token_id = sp.nat(1)
@@ -131,10 +212,21 @@ def test():
             end_time = sp.timestamp(10),
             price_increment = sp.tez(1),
             current_price = sp.tez(0),
-            highest_bidder = bob.address
+            highest_bidder = bob
         )
-    sc += auc.create_auction(auc_data).run(sender = bob.address)
+    sc += auc.create_auction(auc_data).run(sender = bob)
+    
     sc.h1("Bid")
-    sc += auc.bid(1).run(sender = elon.address, amount=sp.tez(1))
+    sc += auc.bid(1).run(sender = elon, amount=sp.tez(1))
+    sc += auc.bid(0).run(sender = elon, amount=sp.tez(1))
+    sc += auc.bid(0).run(sender = bob, amount=sp.tez(2))
+    sc += auc.bid(0).run(sender = mark, amount=sp.tez(3))
+    sc += auc.bid(0).run(sender = elon, amount=sp.tez(3), valid=False)
+    sc += auc.bid(0).run(sender = admin, amount=sp.tez(5))
+    
     sc.h1("Cancel Auction")
-    sc += auc.cancel_auction(sp.nat(1)).run(sender = bob.address)
+    sc += auc.cancel_auction(sp.nat(1)).run(sender = bob)
+    
+    sc.h1("Settle Auction")
+    sc += auc.settle_auction(sp.nat(0)).run(sender = alice)
+    sc += auc.toggle_pause().run(sender = admin)
